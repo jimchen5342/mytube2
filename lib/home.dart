e3b12113be66ef042798b186d5ca1206a749fc55
jim <jimc@bethel.com.tw>
2024-04-02 17:49:59 +0800

    2024-04-02

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+ @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+}
