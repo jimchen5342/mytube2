5f7abb9d0002c87241b6a3985d3bbb4614d884d3
jim <jimc@bethel.com.tw>
2024-04-10 17:50:46 +0800

    2024-04-10

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
-
+import 'package:external_path/external_path.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 
 class Home extends StatefulWidget {
@@ -13,67 +14,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool permission = false;
 
  @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      var status = await Permission.storage.status;
-      if (status.isGranted) {
-        // 還沒寫
-      }
+      await initial();
     });
+  }
+
+  initial() async {
+    final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+    AndroidDeviceInfo build = await deviceInfoPlugin.androidInfo;
+
+    Map<Permission, PermissionStatus> statuses = await [
+          build.version.sdkInt < 30 
+          ? Permission.storage
+          : Permission.manageExternalStorage
+    ].request();
+    var status = build.version.sdkInt < 30 
+      ? await Permission.storage.status
+      : await Permission.manageExternalStorage.status;
+    print("storage: " + status.isGranted.toString());
+    permission = status.isGranted;
+    setState(() {});
+
+    writeFile();
+
+  }
+
+  writeFile() async { // 測好了，可以用
+    var path = await ExternalPath.getExternalStorageDirectories();
+    var file = File('${path[0]}/counter.txt');
+
+    file.writeAsString('jim'); 
+    /* 測好了，可以用
+    var path = await ExternalPath.getExternalStorageDirectories();
+    print("path: ${path[0]}");
+    var path2 = await ExternalPath.getExternalStoragePublicDirectory(ExternalPath.DIRECTORY_MUSIC);
+    print("DIRECTORY_MUSIC: ${path2}");
+    */
   }
 
   @override
   void reassemble() async { // develope mode
     super.reassemble();
-    var status = await Permission.manageExternalStorage.status; // Android 11 (API 30)
-      if (status.isGranted) {
-        // 還沒寫
-        
-      }
-      print("manageExternalStorage: " + status.isGranted.toString());
-
-      final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-      Map<String, dynamic> _deviceData = <String, dynamic>{};
-      print(_readAndroidBuildData(await deviceInfoPlugin.androidInfo),);
-
-  }
-
-  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
-    // https://pub.dev/packages/device_info_plus/example
-    // version.sdkInt: 25, version.release: 7.1.1
-    return <String, dynamic>{
-      'version.securityPatch': build.version.securityPatch,
-      'version.sdkInt': build.version.sdkInt,
-      'version.release': build.version.release,
-      'version.previewSdkInt': build.version.previewSdkInt,
-      'version.incremental': build.version.incremental,
-      'version.codename': build.version.codename,
-      'version.baseOS': build.version.baseOS,
-      'board': build.board,
-      'bootloader': build.bootloader,
-      'brand': build.brand,
-      'device': build.device,
-      'display': build.display,
-      'fingerprint': build.fingerprint,
-      'hardware': build.hardware,
-      'host': build.host,
-      'id': build.id,
-      'manufacturer': build.manufacturer,
-      'model': build.model,
-      'product': build.product,
-      'supported32BitAbis': build.supported32BitAbis,
-      'supported64BitAbis': build.supported64BitAbis,
-      'supportedAbis': build.supportedAbis,
-      'tags': build.tags,
-      'type': build.type,
-      'isPhysicalDevice': build.isPhysicalDevice,
-      'systemFeatures': build.systemFeatures,
-      'serialNumber': build.serialNumber,
-      // 'isLowRamDevice': build.isLowRamDevice,
-    };
+    initial();
   }
 
   @override
@@ -87,8 +74,9 @@ class _HomeState extends State<Home> {
   }
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('單腳擺動',
-            style: TextStyle( color:Colors.white,)
-    ));
+    return Container(
+      color: Colors.white,
+      child: (permission == false)  ? null : Text('MyTube')
+    );
   }
 }
