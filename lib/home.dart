ed75ca7e2aa93b59cc89b526d27f2d07499c5411
jim <jimc@bethel.com.tw>
2024-04-10 09:08:12 +0800

    2024-04-10

--- lib/home.dart
+++ lib/home.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -34,11 +34,45 @@ class _HomeState extends State<Home> {
         
       }
       print("manageExternalStorage: " + status.isGranted.toString());
-      
+
       final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
       Map<String, dynamic> _deviceData = <String, dynamic>{};
-      print(_deviceData);
+      print(_readAndroidBuildData(await deviceInfoPlugin.androidInfo),);
 
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    // https://pub.dev/packages/device_info_plus/example
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'systemFeatures': build.systemFeatures,
+      'serialNumber': build.serialNumber,
+      // 'isLowRamDevice': build.isLowRamDevice,
+    };
   }
 
   @override
