b65c4b761b79e062f76fee0b78b19db7c9d2141b
jim <jimc@bethel.com.tw>
2024-04-19 13:28:54 +0800

    2024-04-19

--- lib/backup/empty.dart
+++ lib/backup/empty.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-class Home extends StatefulWidget {
-  Home({Key? key}) : super(key: key);
+class Empty extends StatefulWidget {
+  Empty({Key? key}) : super(key: key);
   @override
-  _HomeState createState() => _HomeState();
+  _EmptyState createState() => _EmptyState();
 }
 
-class _HomeState extends State<Home> {
+class _EmptyState extends State<Empty> {
 
   @override
   void initState() {
